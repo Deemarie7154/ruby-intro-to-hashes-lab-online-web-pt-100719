6a6d771efea60295d4c8af1a7688b22b9803c6d1
Dwight Marie <dwightmarie@gmail.com>
2019-10-17 00:35:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,16 +38,10 @@ def monopoly_with_third_tier
 	    :names => {},
 	    rent_in_dollars: {
 	      
-	     monopoly = {
-	  railroads: {
-	    :pieces => 4,
-	    :names => {},
-	    :rent_in_dollars => {:one_piece_owned => 25 }
+	   
 	  }
 }	
-end }
-	  }
-}	
+}
 end
 
 
